8ddd89fe52ab5e312a44cc640f791167144585e0
Amit Sharma <akps1440236@gmail.com>
2025-10-02 17:01:51 +0530

    fix: email verification setup

--- backend/src/config/resend.setup.js
+++ backend/src/config/resend.setup.js
@@ -3,7 +3,14 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const resend = new Resend('re_WAPvH4Bq_9xTZ7zwMWA8fByrRE4BwcPkr');
+const resend = new Resend(process.env.RESEND_API_KEY || "");
+if (!resend) {
+  return res.status(500).json({
+    message: 'Resend API key is not set',
+    status: 500,
+    success: false,
+  });
+}
 
 const sendEmail = async (mailOptions) => {
   const { data, error } = await resend.emails.send({
